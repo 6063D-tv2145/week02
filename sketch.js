e1a6ddff72b41d2d2e53489d6e38302d2de0ac59
Thao Chi Vu <tv2145@nyu.edu>
2023-09-13 15:24:16 -0400

    Canvas, 5 elipses, stroke weight and color change

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
 function setup() {
+    createCanvas(1080, 1000);
+    background(204,204,255);    
 }
 
 function draw() {
+    strokeWeight(0)
+    fill(0,0,255)
+    ellipse(300, 100, 100, 100)
+    fill(255,255,204)
+    ellipse(400, 500, 100, 100)
+    strokeWeight(4)
+    stroke(255,204,229)
+    ellipse(350, 300, 100, 100)
+    ellipse(100, 400, 100, 100)
+    ellipse(700, 200, 100, 100)
+    rect(300, 100, 50, 50)
 }
