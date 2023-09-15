b81da8eada11bd19280767e96a665f44ffadf9df
Thao Chi Vu <tv2145@nyu.edu>
2023-09-15 08:24:42 -0400

    pink heart, cursor shape, corner items

--- sketch.js
+++ sketch.js
@@ -1,18 +1,23 @@
 
 function setup() {
-    createCanvas(1080, 1000);
-    background(204,255,229);    
+    createCanvas(1000, 1000);
 }
 
 function draw() {
-    fill("pink")
-    strokeWeight(0)
-    ellipse(200, 300, 300, 300)
-    ellipse(450, 300, 300, 300)
-    rotate(PI / 4.0)
-    rect(380, -82, 300, 300)
-    fill("beige")
-    strokeWeight(0)
-    square(450, 30, 55, 20, 15, 10, 5);
+    background(204,255,229);    
+    fill(0);
+    ellipse (mouseX, mouseY, 100, 100);
+    fill(0);
+    rect(0, 0, 50, 50);
+    fill (0);
+    rect(width-50, 0, 50,50);
+    rect(width-50, height-50, 50,50);
+    rect(0, height-50, 50,50);
+    fill("pink");
+    strokeWeight(0);
+    ellipse(200, 300, 300, 300);
+    ellipse(450, 300, 300, 300);
+    rotate(PI / 4.0);
+    rect(380, -82, 300, 300);
 
 }
